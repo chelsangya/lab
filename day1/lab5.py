0e455b25698ec39a9b95c0af0e16c86ce631df78
chelsangya <csangya@gmail.com>
2021-12-03 12:25:33 +0545

    day1

--- day1/lab5.py
+++ day1/lab5.py
@@ -2,5 +2,3 @@
 radius=int(input("enter the radius of a circle"))
 area= 22/7 *(radius**2)
 print("the area of the circle is {} " .format(area))
-#add even numbers of first positive 10 numbers
-#add odd numbers of first positive 10 numbers
