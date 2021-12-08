49d189d099e46aa5f8f0bf85225d2d08e25fd71b
chelsangya <csangya@gmail.com>
2021-12-08 12:18:09 +0545

    day4

--- day3/lab7.py
+++ day3/lab7.py
@@ -1,5 +1,5 @@
 #Given a positive real number, print its fractional part. 
 a= float(input("enter the number"))
 integer=int(a)
-frac= a- integer
+frac= a-integer
 print(f"integer: {integer}  decimal: {frac} ")
---
+++ day4/lab1.py
@@ -0,0 +1,12 @@
+# Check whether the given year is leap year or not. If year is leap print ‘LEAP YEAR’ elseprint ‘COMMON YEAR’.
+# Hint: • a year is a leap year if its number is exactly divisible by 4 and is not exactly divisible by 100
+# • a year is always a leap year if its number is exactly divisible by 400
+year= int(input("Enter the year "))
+mod400= year%400
+mod100=year%100
+mod4=year%4
+if mod4==0 and mod100 !=0 and mod400==0:
+    value="LEAP YEAR"
+else:
+    value="COMMON YEAR"
+print(f"The given year is a {value}")
