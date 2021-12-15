0f8ae08b43a836bd039093d711458001b150eb64
chelsangya <csangya@gmail.com>
2021-12-15 13:27:56 +0545

    day5

---
+++ day5/lab6.py
@@ -0,0 +1,7 @@
+#count the number of even and odd numbers from a series of numbers
+for i in range(0,101):
+    if i%2==0:
+        print(f"{i} is an  even number")
+    else:
+        print(f"{i} is an odd number")
+        
